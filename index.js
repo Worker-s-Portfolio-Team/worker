34f06da8259e5cd6d6d3e477c5ef9a6ec0bd1675
daservajesus <16702642+daservajesus@users.noreply.github.com>
2021-04-12 21:06:39 +0000

    Change margins to padding

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ function render(results) {
 */
 
 function render(results) {
-    let list = '<div class="container mt-3">';
+    let list = '<div class="container py-5">';
     list += '<div class="row">';
     for (let result of results.results) {
         let now = new Date(result.start),
