09a9d9d73304eb423abe6ad54e946bc4815a6324
ArbogastUnafraidFidelity <37487746+alastairtechnologies@users.noreply.github.com>
2021-04-12 15:13:30 -0700

    removed comments

--- index.js
+++ index.js
@@ -3,32 +3,10 @@ addEventListener("fetch", (event) => {
 });
   
 async function handleRequest(event) {
-    let data = dataFetch(event)
+    let data = await dataFetch(event);
     data = render(await data.json());
     return new HTMLRewriter().on("section", new ElementHandler(data)).transform(await fetch("https://concert.goalastair.com"));
 }
-  
-/* 
-function render(results) {
-    let list = "<div class=\"card-group\">";
-    for (let result of results.results) {
-        let now = new Date(result.start),
-        badges = "";
-        for(let label of result.labels)
-            badges += `<span class="badge badge-info">${label}</span>&nbsp;`;
-        date = `${now.getMonth()+1}/${now.getDate()}/${now.getFullYear()}`;
-        list += `<div class="card text-center">
-            <div class="card-body">
-                <a href="https://events.predicthq.com/events/${result.id}">${result.title}</a>
-                <p class="card-text">Begins on ${date}</p>
-                ${badges}
-            </div>
-        </div>`;
-    }
-    list += "</div>";
-    return list;
-} 
-*/
 
 async function dataFetch(event) {
     const fetchURL = `https://api.predicthq.com/v1/events/?category=concerts%2Cfestivals&country=${event.request.cf.country}`;
