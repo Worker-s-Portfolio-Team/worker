71da8365b144008d714f50c75e38294249fb51e0
ArbogastUnafraidFidelity <37487746+alastairtechnologies@users.noreply.github.com>
2021-04-12 12:34:10 -0700

    final push

--- index.js
+++ index.js
@@ -2,20 +2,27 @@ addEventListener("fetch", (event) => {
     event.respondWith(handleRequest(event.request));
 });
   
-async function handleRequest(request) {
-    let req = new Request(`https://api.predicthq.com/v1/events/?category=concerts%2Cfestivals&country=${request.cf.country}`);
-    req.headers.set("Authorization", AUTH);
-    let res = await fetch(req),
-        results = await res.json();
-    return render(results);
+async function handleRequest(req) {
+    let url = new URL(req.url);
+    let data = new Request(`https://api.predicthq.com/v1/events/?category=concerts%2Cfestivals&country=${req.cf.country}`);
+    data.headers.set("Authorization", AUTH);
+    let res = await fetch(data);
+    data = render(await res.json());
+    return new HTMLRewriter().on("ul", new ElementHandler(data)).transform(await fetch("https://concert.goalastair.com"));
 }
   
-async function render(results) {
-    let list = "<ul class=\"list-group\">";
+function render(results) {
+    let list = "";
     for (let result of results.results) list += `<li class="list-group-item"><a href="https://events.predicthq.com/events/${result.id}">${result.title}</a></li>`;
-    list += "</ul>";
-    console.log(list);
-    let res = new Response(list);
-    res.headers.set("content-type", "text/html");
-    return res;
-}  
+    return list;
+} 
+
+class ElementHandler {
+    constructor(data) {
+      this.data = data;
+    }
+    element(element) {
+      console.log(this.data);
+      element.setInnerContent(this.data, {html: true})
+    }
+  }
