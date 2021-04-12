0b1c3be76b31a5e5e683d59c52cd58501501fa73
ArbogastUnafraidFidelity <37487746+alastairtechnologies@users.noreply.github.com>
2021-04-12 11:42:26 -0700

    added list generation

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-const concertPage = require("./lib/render.js");
-addEventListener('fetch', event => {
+addEventListener("fetch", (event) => {
     event.respondWith(handleRequest(event.request));
 });
-
+  
 async function handleRequest(request) {
-    const page = new concertPage(await fetch(request));
-    const results = await fetch(`https://api.songkick.com/api/3.0/metro_areas/${request.cf.metroCode}/calendar.json?apikey=${SongKickKey}`);
-    response = await page.render(results);
-    return response;
+    let req = new Request(`https://api.predicthq.com/v1/events/?category=concerts%2Cfestivals&country=${request.cf.country}`);
+    req.headers.set("Authorization", AUTH);
+    let res = await fetch(req),
+        results = await res.json();
+    return render(results);
 }
+  
+async function render(results) {
+    let list = "<ul class=\"list-group\">";
+    for (let result of results.results) list += `<li class="list-group-item"><a href="https://events.predicthq.com/events/${result.id}">${result.title}</a></li>`;
+    list += "</ul>";
+    console.log(list);
+    let res = new Response(list);
+    res.headers.set("content-type", "text/html");
+    return res;
+}  
